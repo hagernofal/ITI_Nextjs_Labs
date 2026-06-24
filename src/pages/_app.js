86380341b0f65b37e10f299948b8b041e354ebf2
hagernofal <hagernofal8@gmail.com>
2026-06-24 17:39:23 +0300

    update global app configuration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import {useEffect} from "react";
 import { useRouter } from "next/router";
 import NavBar from "@/components/NavBar";
-export default function App({ Component, pageProps }) {
+import { SessionProvider } from "next-auth/react";
+export default function App({ Component, pageProps:{session,...pageProps} }) {
   const router = useRouter();
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap.bundle.min.js");
@@ -11,10 +12,10 @@ export default function App({ Component, pageProps }) {
   const hideNavbarRoutes = ["/404", "/_error"];
   const shouldHideNavbar = hideNavbarRoutes.includes(router.pathname);
   return (
-    <>
+    <SessionProvider session={session}>
     {!shouldHideNavbar && <NavBar />}
     <Component {...pageProps}/>
-    </>
+    </SessionProvider>
   
 );
 }
