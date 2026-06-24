b810b97ba685a8a54f70a7fbd72c2b6fe9b18cec
hagernofal <hagernofal8@gmail.com>
2026-06-24 17:38:38 +0300

    update navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
+import { signOut , useSession } from 'next-auth/react';
 
 const NavBar = () => {
+    const {data:session} = useSession()
+    if(session){
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
             <div className="container-fluid">
@@ -12,16 +15,17 @@ const NavBar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link" href="/">Home</Link>
+                            <Link className="nav-link" href="/products">Products</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" href="/products">Products</Link>
+                            <Link className="nav-link" href="/api/auth/signout" onClick={()=> signOut}>sign out</Link>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
     );
+}
 };
 
 export default NavBar;
